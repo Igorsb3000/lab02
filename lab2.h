a7b0fa2ae0a823c56abfcfd5d96cd15e9ee61a6d
Igorsb3000 <igorsb3000@gmail.com>
2019-03-21 21:45:01 -0300

    Create lab2.h

---
+++ lab2.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class FUNCIONARIO{
+	private:
+		string nome;
+		int salario;
+		string admissao;
+	public:
+		void setnome(string nome);
+		void setsalario(int salario);
+		void  setadmissao(string admissao);
+
+
+
+}
