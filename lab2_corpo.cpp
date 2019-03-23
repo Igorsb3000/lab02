d59c9d8ae0f28ad71c4c0c06556a16c8c357ac15
Igorsb3000 <igorsb3000@gmail.com>
2019-03-23 10:09:38 -0300

    atualizacao

--- lab2_corpo.cpp
+++ lab2_corpo.cpp
@@ -1,1 +1,13 @@
 #include "lab2.h"
+
+
+void funcionario::setNome(string n){
+	nome = n;
+}
+
+void funcionario::setSalario(int s){
+	salario = s;
+}
+
+void funcionario::setAdmissao(int admissao[], int tamanho){
+	int 
