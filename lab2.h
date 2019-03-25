e271265f6291b4bbd41b469d22d942a327a078b8
Igorsb3000 <igorsb3000@gmail.com>
2019-03-25 14:38:08 -0300

    Construtor de funcionarios

--- lab2.h
+++ lab2.h
@@ -2,39 +2,48 @@
 #include <string>
 #include <cstring>
 #include <list>
+//#include <vector>
+//#include <algorithm>
 
 
 
 using namespace std;
 
 
-
+//a)
 class funcionario{
 	private:
 		string nome;
 		double salario;
 		int *admissao;
 	public:
+		//Costrutores
+		funcionario();
+		funcionario(string nome, double salario, int *admissao);
+		//Métodos setters
 		void setNome(string n);
 		void setSalario(double s);
 		void setAdmissao(int admissao[]);
-
+		//Métodos getters
 		string getNome();
 		double getSalario();
 		int* getAdmissao();
 
 };
-
+//b)
 class empresa{
 	private:
 		string razao;
 		int cnpj;
-		list<funcionario>colaboradores;
+		list <funcionario> colaboradores;
 	public:
-		string getRazao();
+		//Métodos setters
 		void setRazao(string);
-		int getCnpj();
 		void setCnpj(int);
-		funcionario* getColaborador(string);
 		void setColaborador(funcionario);
+		//Métodos getters
+		string getRazao();
+		int getCnpj();
+		funcionario* getColaborador(string);
+		
 };
