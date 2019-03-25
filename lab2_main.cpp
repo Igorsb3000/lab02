b216d225aedf46654be0bb1eba02f7c9cd9d96f6
Igorsb3000 <igorsb3000@gmail.com>
2019-03-25 14:39:27 -0300

    Construtor de funcionarios

--- lab2_main.cpp
+++ lab2_main.cpp
@@ -2,32 +2,18 @@
 
 int main(){
 	funcionario R;
-	funcionario vetor[3];
+	
+	//c)
 	empresa A;
-	string nome_main;
 	string razao;
 	int cnpj;
+
+
+	string nome_main;
 	double salario_main;
-	int admissao_main[3];
+	int *admissao_main = new int[3];
 
-	cout << "Digite o nome do funcionario: " << endl;
-	//cin.ignore();
-	getline(cin, nome_main);
-
-	cout << "Digite o salario do funcionario: " << endl;
-	cin >> salario_main;
-
-	cout << "Digite a data de admissao do funcionario: ";
-	cin >> admissao_main[0] >> admissao_main[1] >> admissao_main[2];
-
-	for(int i = 0; i<3; i++){
-		cout << admissao_main[i] << endl;
-	}
-	R.setNome(nome_main);
-	R.setSalario(salario_main);
-	R.setAdmissao(admissao_main);
-	
-
+	/*
 	cout << "Funcionario R:" << endl;
 	cout << "Nome: " << R.getNome() << endl;
 	cout << "Salario: " << R.getSalario() << endl;
@@ -36,14 +22,51 @@ int main(){
 	cout << "Ano: " << R.getAdmissao()[2] << endl;
 	cout << endl << endl;
 	cout << "Digite a razao social da empresa:" << endl;
-	cin >> razao;
-
+	cin >> razao; 
+	
 	A.setRazao(razao);
 	cout << "Digite o CNPJ da empresa:" << endl;
 	cin >>  cnpj;
 	A.setCnpj(cnpj);
-	A.setColaborador(R);	
-	cout << endl << endl;
+	//A.setColaborador(R);	
+	cout << endl << endl; */
+
+
+
+	char resposta;
+	int contador=0;
+
+	//d)
+	do{
+		cout << "Deseja adicionar um novo funcinário ? s/n" << endl;
+		cin >> resposta;
+
+		if(resposta != 'n'){
+			cout << "Digite o nome do funcionario: " << endl;
+			cin.ignore();
+			getline(cin, nome_main);
+
+			cout << "Digite o salario do funcionario: " << endl;
+			cin >> salario_main;
+
+			cout << "Digite a data de admissao do funcionario: ";
+			cin >> admissao_main[0] >> admissao_main[1] >> admissao_main[2];
+
+			funcionario R(nome_main, salario_main, admissao_main);
+
+			A.setColaborador(R);
+			contador++;
+
+		}
+
+
+	}while(resposta != 'n');
+
+	//for(int i=0; i<A->colaboradores.size(); i++){
+	//	cout << A.colaboradores.front() << endl;
+	//}
+
+	
 
 	cout << "Digite o nome do funcionario que deseja pesquisar:";
 	cin >> nome_main;
@@ -54,6 +77,8 @@ int main(){
 		cout << "Salario: " << A.getColaborador(nome_main)->getSalario() << endl;
 		cout << "Data de admissao:" << A.getColaborador(nome_main)->getAdmissao()[0] << "/" << A.getColaborador(nome_main)->getAdmissao()[1] << "/" << A.getColaborador(nome_main)->getAdmissao()[2] << endl;
 	}
+
+	delete[] admissao_main;
 	
 	return 0;
 }
